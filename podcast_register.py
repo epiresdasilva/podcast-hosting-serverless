315cf79cc80a2c4d3cf942da447f14557a35744a
Evandro Pires da Silva <evandro@evandropires.com.br>
2020-05-23 06:49:54 -0300

    Podcast register

--- podcast_register.py
+++ podcast_register.py
@@ -4,22 +4,24 @@ from urllib.parse import unquote_plus
 import uuid
 from mp3_tagger import MP3File
 
-
 s3_client = boto3.client('s3')
 
 
 def main(event, context):
+    response = []
+
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = unquote_plus(record['s3']['object']['key'])
         tmpkey = key.replace('/', '')
         download_path = '/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
         s3_client.download_file(bucket, key, download_path)
-        extract_metadata(download_path)
+        response.append(extract_metadata(download_path))
 
     body = {
         "message": "Registering podcast!",
-        "input": event
+        "input": event,
+        "response": response
     }
 
     response = {
@@ -32,4 +34,18 @@ def main(event, context):
 
 def extract_metadata(file_path):
     mp3 = MP3File(file_path)
-    print(mp3.album)
+
+    dynamodb = boto3.resource('dynamodb', region_name='sa-east-1')
+
+    table = dynamodb.Table('semservidor-dev')
+
+    return table.put_item(
+        Item={
+            'id': mp3.song[0].value[1:2],
+            'info': {
+                'episodio': mp3.song[0].value,
+                'descricao': mp3.comment[0].value,
+                'ano': mp3.year[0].value
+            }
+        }
+    )

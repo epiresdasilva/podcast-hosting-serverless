3054b82cce8e75361e4d587f59853bc8e0a2b92d
Evandro Pires da Silva <evandro@evandropires.com.br>
2020-05-23 09:39:27 -0300

    Register podcast file and size

--- podcast_register.py
+++ podcast_register.py
@@ -3,6 +3,8 @@ import boto3
 from urllib.parse import unquote_plus
 import uuid
 from mp3_tagger import MP3File
+from pathlib import Path
+
 
 s3_client = boto3.client('s3')
 
@@ -16,7 +18,7 @@ def main(event, context):
         tmpkey = key.replace('/', '')
         download_path = '/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
         s3_client.download_file(bucket, key, download_path)
-        response.append(extract_metadata(download_path))
+        response.append(extract_metadata(download_path, key))
 
     body = {
         "message": "Registering podcast!",
@@ -32,12 +34,13 @@ def main(event, context):
     return response
 
 
-def extract_metadata(file_path):
+def extract_metadata(file_path, key):
     mp3 = MP3File(file_path)
 
     dynamodb = boto3.resource('dynamodb', region_name='sa-east-1')
 
     table = dynamodb.Table('semservidor-dev')
+    file_size = Path(file_path).stat().st_size
 
     return table.put_item(
         Item={
@@ -45,7 +48,11 @@ def extract_metadata(file_path):
             'info': {
                 'episodio': mp3.song[0].value,
                 'descricao': mp3.comment[0].value,
-                'ano': mp3.year[0].value
+                'ano': mp3.year[0].value,
+                'arquivo': {
+                    'nome': key,
+                    'tamanho': file_size
+                }
             }
         }
     )
